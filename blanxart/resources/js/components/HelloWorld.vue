e84d92cb32c36e52198b71f88d88bb91b9ff099e
amateos <amateos@alumnat.copernic.cat>
2024-03-20 17:16:14 +0100

    instalar con vite sass y vue

---
+++ blanxart/resources/js/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="hello">
+      <h1>{{ greeting }}</h1>
+      <p>This is a Vue component!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        greeting: 'Hello, world!'
+      };
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .hello {
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+  }
+  </style>
