0e7d5e421384682c33b68b0dfc2752d38ac4da95
Andres <arojas@alumnat.copernic.cat>
2024-03-20 20:02:13 +0100

    migraciones hechas

---
+++ blanxart/app/Models/Cita.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Cita extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'fecha',
+        'nivel_emergencia',
+        'aceptada',
+        'realizada',
+        'prueba_id'
+    ];
+
+    public function prueba(): BelongsTo
+    {
+        return $this->belongsTo(Prueba::class);
+    }
+
+    public function resultado(): HasOne
+    {
+        return $this->hasOne(Resultado::class);
+    }
+}
--- blanxart/app/Models/User.php
+++ blanxart/app/Models/User.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable
 {
@@ -41,4 +44,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function paciente(): HasOne
+    {
+        return $this->hasOne(Paciente::class);
+    }
+
+    public function medico(): HasOne
+    {
+        return $this->hasOne(Medico::class);
+    }
+
+    public function administrador(): HasOne
+    {
+        return $this->hasOne(Administrador::class);
+    }
 }
