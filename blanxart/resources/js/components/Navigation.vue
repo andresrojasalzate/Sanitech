67893de65eab32f3e4b707847317033fe458123a
SamyTheLight <62912197+SamyTheLight@users.noreply.github.com>
2024-04-04 17:49:59 +0200

    Header fixed

--- blanxart/resources/js/components/Navigation.vue
+++ blanxart/resources/js/components/Navigation.vue
@@ -1,38 +1,38 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light">
-      <div class="container">
-        <div class="navbar-brand" to="/">
-            <div class="logo"></div>
-            Sanitech
-        </div>
-        <button class="navbar-toggler icon" type="button" @click="toggleNavbar">
-          <i class="fas fa-bars"></i>
-        </button>
-        <div :class="['collapse', { 'show': isNavbarOpen }]" class="navbar-collapse">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="/">Inicio</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/agenda">Agenda</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/citas">Citas</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="">Solicitudes</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="">Notificaciones</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="">Logout</a>
-            </li>
-          </ul>
-        </div>
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <div class="container">
+      <div class="navbar-brand" to="/">
+        <div class="logo"></div>
+        Sanitech
       </div>
-    </nav>
-  </template>
+      <button class="navbar-toggler icon" type="button" @click="toggleNavbar">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div :class="['collapse', { 'show': isNavbarOpen }]" class="navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/agenda">Agenda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/citas">Citas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">Solicitudes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">Notificaciones</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
   
   <script>
   export default {
