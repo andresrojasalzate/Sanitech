14e56e52a7d82967180f58db5beb9d78823aaeed
SamyTheLight <62912197+SamyTheLight@users.noreply.github.com>
2024-04-10 16:51:28 +0200

    diseño footer

--- blanxart/resources/views/partials/footer.blade.php
+++ blanxart/resources/views/partials/footer.blade.php
@@ -1,5 +1,12 @@
 <footer class="footer">
-    <a class="footer-link" href="#">Home</a>
-    <a class="footer-link" href="#">Sobre Nosotros</a>
-    <a class="footer-link" href="#">Avisos Legales</a>
+    <div class="footer-logos-instituto">
+        <img src="{{asset('storage/images/blanxart-logo.png')}}">
+        <img src="{{asset('storage/images/nicolau-copernic-logo.png')}}">
+    </div>
+    <p>Catalunya</p>
+    <div class="footer-copyright">
+        <i class="fa-regular fa-copyright"></i>
+        <p>Sanitech - Todos los derechos reservados</p>
+    </div>
+    <p>2024</p>
 </footer>
