58171ffd3bff29824656a16a1908a2a9f0d5c631
amateos <amateos@alumnat.copernic.cat>
2024-04-18 17:36:39 +0200

    corregir error del paginador de agenda

--- blanxart/app/Models/Cita.php
+++ blanxart/app/Models/Cita.php
@@ -47,7 +47,7 @@ class Cita extends Model
     {
         $citas = DB::table('citas')
             ->join('pruebas', 'citas.prueba_id', '=', 'pruebas.id')
-            ->select('citas.*', 'pruebas.*')
+            ->select('citas.*', 'pruebas.name', 'pruebas.video', 'pruebas.document')
             ->where('citas.paciente_id', $id)
             ->get();
 
