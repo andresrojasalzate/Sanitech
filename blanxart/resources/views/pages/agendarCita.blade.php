e2c74f2cd65c871b21c45c2fac9e1cfec84131b4
amateos <amateos@alumnat.copernic.cat>
2024-04-25 17:49:34 +0200

    crear formulario para agendar la cita al paciente y enviar notificacion para que el paciente confirme la cita

--- blanxart/resources/views/pages/agendarCita.blade.php
+++ blanxart/resources/views/pages/agendarCita.blade.php
@@ -4,5 +4,19 @@
 @section('descripcion_pagina', 'Pantalla para asignar citas solicitadas por medicos')
 
 @section('content')
-hola
+
+{{-- @dd($medicos) --}}
+<form action="{{ route('cita.actualizar', $cita_id) }}" method="POST" class="form-container">
+    @csrf
+    <div id="agendarCita">
+        <agendar-citas-component :cita_id='{{$cita_id}}' :medicos='{{$medicos}}'></agendar-citas-component>
+    </div>
+    @error('error')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+    <button type="submit" class="btn-submit">Enviar</button>
+</form>
+
+
+
 @endsection
--- blanxart/routes/web.php
+++ blanxart/routes/web.php
@@ -49,7 +49,9 @@ Route::group(['middleware' => 'auth'], function () {
     
     Route::group(['middleware' => ['rol:admin']], function () {
         Route::get('/asignarFechaCita', [PedirCitaController::class, 'asignarFechaCita'])->name('asignarFechaCita');
-        Route::get('/asignarFechaCita/cita/{id}', [PedirCitaController::class, 'agendarCita'])->name('asignarFechaCita');
+        Route::get('/asignarFechaCita/cita/{id}', [PedirCitaController::class, 'agendarCita'])->name('agendarCita');
+        Route::post('/actualizar-cita/{id}', [PedirCitaController::class, 'actualizarCita'])->name('cita.actualizar');
+
         
     });
 
