7f18427e034602e0d6b044014041dbcf0a52b10e
amateos <amateos@alumnat.copernic.cat>
2024-05-03 16:25:57 +0200

    cambiar a catalan algunas palabras/frases

--- blanxart/resources/views/partials/footer.blade.php
+++ blanxart/resources/views/partials/footer.blade.php
@@ -6,7 +6,7 @@
     <p>Catalunya</p>
     <div class="footer-copyright">
         <i class="fa-regular fa-copyright"></i>
-        <p>Sanitech - Todos los derechos reservados</p>
+        <p>Sanitech - Tots els drets reservats</p>
     </div>
     <p>2024</p>
 </footer>
