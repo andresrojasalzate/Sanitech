26f4743f1cb83d0ab46368e86bdbec4c0f600101
Diego Yesid Amador Hernandez <dyamador@alumnat.copernic.cat>
2024-05-07 17:23:07 +0000

    ajuste metodo show del InformesClinicosController para el error de informes en los pacientes

--- blanxart/app/Models/Resultado.php
+++ blanxart/app/Models/Resultado.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Resultado extends Model
 {
@@ -17,5 +19,18 @@ class Resultado extends Model
     public function paciente(): BelongsTo
     {
         return $this->belongsTo(Paciente::class);
+    }
+
+    public static function getMedicalResults($id)
+    {
+        Log::info('Llamada al metodo Resultado.getMedicalResults id = '. $id);
+        
+        $resultados = DB::table('resultados')
+                        ->join('pacientes','pacientes.id','=','resultados.paciente_id')
+                        ->join('users','users.id','=','pacientes.user_id')
+                        ->where('users.id','=',$id)
+                        ->paginate(10);
+
+        return $resultados;
     }
 }
