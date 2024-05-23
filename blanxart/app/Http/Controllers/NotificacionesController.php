82c2734ef288e5ea70a99f385208467a3844b2ad
amateos <amateos@alumnat.copernic.cat>
2024-05-23 17:21:35 +0200

    crear nuevo tipo de notificacion y ponerlo en la pantalla de notificaciones, hacer tambien que aparezca la cuenta en el home

--- blanxart/app/Http/Controllers/NotificacionesController.php
+++ blanxart/app/Http/Controllers/NotificacionesController.php
@@ -6,7 +6,9 @@ use App\Models\Cita;
 use App\Models\Paciente;
 use App\Models\Notificacion;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use App\Models\NotificacionPaciente;
 
 class NotificacionesController extends Controller
 {
@@ -14,21 +16,67 @@ class NotificacionesController extends Controller
     {
 
         $paciente_id = Paciente::where('user_id', $id)->value('id');
-        $notificaciones = Notificacion::where('paciente_id', $paciente_id)
+        $notificacionesCita = Notificacion::where('paciente_id', $paciente_id)
             ->join('citas', 'citas.id', '=', 'notificacions.cita_id')
             ->select('notificacions.id', 'notificacions.cita_id', 'notificacions.title', 'citas.accepted', 'notificacions.descripcion', 'notificacions.tipo', 'notificacions.created_at', 'notificacions.vista')
             ->orderBy('notificacions.created_at', 'desc')
             ->get();
+        $notificacionesCitaPaciente = DB::table('notificacion_pacientes')
+            ->get();
 
 
 
-        foreach ($notificaciones as $notificacion) {
+        $notificaciones = [];
+        $id = 1;
+        foreach ($notificacionesCita as $notificacion) {
+
+            $notificaciones[] = [
+                'id' => $id,
+                'cita_id' => $notificacion->cita_id,
+                'title' => $notificacion->title,
+                'accepted' => $notificacion->accepted,
+                'descripcion' => $notificacion->descripcion,
+                'tipo' => $notificacion->tipo,
+                'created_at' => $notificacion->created_at,
+                'vista' => $notificacion->vista,
+            ];
             $notificacion = Notificacion::find($notificacion->id);
             $notificacion->vista = true;
             $notificacion->save();
+
+            $id++;
         }
 
-        $notificaciones = $notificaciones->toJson();
+        foreach ($notificacionesCitaPaciente as $notificacion) {
+            $notificaciones[] = [
+                'id' => $id,
+                'cita_id' => null,
+                'title' => $notificacion->title,
+                'accepted' => null,
+                'descripcion' => $notificacion->descripcion,
+                'tipo' => $notificacion->tipo,
+                'created_at' => $notificacion->created_at,
+                'vista' => $notificacion->vista,
+            ];
+            $notificacion = NotificacionPaciente::find($notificacion->id);
+            $notificacion->vista = true;
+            $notificacion->save();
+
+            $id++;
+        }
+
+        
+        usort($notificaciones, function ($a, $b) {
+            return strtotime($b['created_at']) - strtotime($a['created_at']);
+        });
+
+        
+        // $notificaciones = $notificaciones->toJson();
+
+        $notificaciones = json_encode($notificaciones);
+
+        // dd($notificaciones);
+
 
         return view('pages.notificaciones', compact('notificaciones'));
     }
